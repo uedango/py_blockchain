4148ccab3c360fcd6618d1e11aa4e3aed8a81c01
uedai <uehome2015@gmail.com>
2020-08-25 18:04:35 +0900

    Create utils Success!

--- blockchain.py
+++ blockchain.py
@@ -1,6 +1,11 @@
+import hashlib
+import json
+
 import logging
 import sys
 import time
+
+import utils
 
 logging.basicConfig(level=logging.INFO, stream=sys.stdout)
 
@@ -10,19 +15,24 @@ class Blockchain(object):
   def __init__(self):
     self.transaction_pool = []
     self.chain = []
-    self.create_block(0, 'init hash')
+    self.create_block(0, self.hash({}))
 
   def create_block(self, nonce, previous_hash):
-    block = {
+    block = utils.sorted_dict_by_key({
       'timestamp': time.time(),
       'transactions': self.transaction_pool,
       'nonce': nonce,
       'previous_hash': previous_hash
-    }
+    })
     self.chain.append(block)
     self.transaction_pool = []
     return block
     
+  def hash(self, block):
+    sorted_block = json.dumps(block, sort_keys=True)
+    return hashlib.sha256(sorted_block.encode()).hexdigest()
+
+
 def pprint(chains):
   for i, chain in enumerate(chains):
     print(f'{"="*25} Chain {i} {"="*25}')
@@ -34,10 +44,12 @@ if __name__ == '__main__':
   block_chain = Blockchain()
   pprint(block_chain.chain)
 
-  block_chain.create_block(5, 'hash 1')
+  previous_hash = block_chain.hash(block_chain.chain[-1])
+  block_chain.create_block(5, previous_hash)
   pprint(block_chain.chain)
 
-  block_chain.create_block(2, 'hash 2')
+  previous_hash = block_chain.hash(block_chain.chain[-1])
+  block_chain.create_block(2, previous_hash)
   pprint(block_chain.chain)
 
 
--- utils.py
+++ utils.py
@@ -1,2 +1,5 @@
-import hashlib
+import collections
 
+
+def sorted_dict_by_key(unsorted_dict):
+  return collections.OrderedDict(sorted(unsorted_dict.items(), key=lambda d:d[0]))

0fb4d179e51ebf0b5c4e26aa2ef38cedf9c7f441
uedai <uehome2015@gmail.com>
2020-09-13 11:36:10 +0900

    26 Create transaction in waller_server. no run

--- wallet_server.py
+++ wallet_server.py
@@ -1,8 +1,13 @@
+from blockchain_server import transaction
+import urllib.parse
+
 from argparse import PARSER
 from ecdsa.ecdsa import Private_key, Public_key
 from flask import Flask
 from flask import jsonify
 from flask import render_template
+from flask import request
+import requests
 
 import wallet
 
@@ -21,6 +26,47 @@ def create_wallet():
     'blockchain_address': my_wallet.blockchain_address,
   }
   return jsonify(response), 200
+
+@app.route('/transaction', methods={'POST'})
+def create_transaction():
+  request_json = request.json
+  required = (
+    'sender_private_key',
+    'sender_blockchain_address',
+    'recipisent_blockchain_address',
+    'sender_public_key',
+    'value')
+  if not all(k in request_json for k in required):
+    return 'missing values', 400
+  
+  sender_private_key = request_json['sender_private_key']
+  sender_blockchain_address = request_json['sender_blockchain_address']
+  recipisent_blockchain_address = request_json['recipisent_blockchain_address']
+  sender_public_key = request_json['sender_public_key']
+  value = float(request_json['value'])
+
+  transaction = wallet.Transaction(
+    sender_private_key,
+    sender_public_key,
+    sender_blockchain_address,
+    recipisent_blockchain_address,
+    value)
+
+  json_data = {
+    'sender_blockchain_address': sender_blockchain_address,
+    'recipisent_blockchain_address': recipisent_blockchain_address,
+    'sender_public_key': sender_public_key,
+    'value': value,
+    'signature': transaction.generate_signature(),
+  }
+  response = requests.post(
+    urllib.parse.urljoin(app.config['gw'], 'transactions'),
+    json=json_data, timeout=3)
+
+  if response.status_code == 201:
+    return jsonify({'message': 'success'}), 201
+  return jsonify({'message': 'fail','response': response}), 400
+
 
 if __name__ == '__main__':
   from argparse import ArgumentParser
